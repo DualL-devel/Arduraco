50c206326d7a130574730458dc72be16165386c5
Jo-Jo0 <jo-jo0698@jandex.ru>
2015-08-16 18:05:16 +0200

    added CMakeLists.txt in datenstruktur

---
+++ ArduracoForPC/ArduracoCLI/build/datenstruktur/CMakeFiles/datenstruktur.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/johannes/ArduRaCo/Arduraco.git/ArduracoForPC/ArduracoCLI/datenstruktur/gleis.cpp" "/home/johannes/ArduRaCo/Arduraco.git/ArduracoForPC/ArduracoCLI/build/datenstruktur/CMakeFiles/datenstruktur.dir/gleis.o"
+  "/home/johannes/ArduRaCo/Arduraco.git/ArduracoForPC/ArduracoCLI/datenstruktur/gleissystem.cpp" "/home/johannes/ArduRaCo/Arduraco.git/ArduracoForPC/ArduracoCLI/build/datenstruktur/CMakeFiles/datenstruktur.dir/gleissystem.o"
+  "/home/johannes/ArduRaCo/Arduraco.git/ArduracoForPC/ArduracoCLI/datenstruktur/licht.cpp" "/home/johannes/ArduRaCo/Arduraco.git/ArduracoForPC/ArduracoCLI/build/datenstruktur/CMakeFiles/datenstruktur.dir/licht.o"
+  "/home/johannes/ArduRaCo/Arduraco.git/ArduracoForPC/ArduracoCLI/datenstruktur/reed.cpp" "/home/johannes/ArduRaCo/Arduraco.git/ArduracoForPC/ArduracoCLI/build/datenstruktur/CMakeFiles/datenstruktur.dir/reed.o"
+  "/home/johannes/ArduRaCo/Arduraco.git/ArduracoForPC/ArduracoCLI/datenstruktur/zug.cpp" "/home/johannes/ArduRaCo/Arduraco.git/ArduracoForPC/ArduracoCLI/build/datenstruktur/CMakeFiles/datenstruktur.dir/zug.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ ArduracoForPC/ArduracoCLI/build/datenstruktur/CMakeFiles/datenstruktur.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/datenstruktur.dir/gleis.o"
+  "CMakeFiles/datenstruktur.dir/gleissystem.o"
+  "CMakeFiles/datenstruktur.dir/licht.o"
+  "CMakeFiles/datenstruktur.dir/reed.o"
+  "CMakeFiles/datenstruktur.dir/zug.o"
+  "libdatenstruktur.pdb"
+  "libdatenstruktur.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/datenstruktur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
